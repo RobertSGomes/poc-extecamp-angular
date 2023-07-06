5640e2515fe80dae3414f371f1272b3c696ab88d
RogerSGomes <rogersg.dev@gmail.com>
2023-07-06 19:29:42 -0300

    FEAT: Student all courses added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { SignupComponent } from './templates/signup/signup.component';
 import { AllCoursesComponent } from './templates/all-courses/all-courses.component';
 import { ProfessorHomeComponent } from './templates/professor-home/professor-home.component';
 import { ProfesssorOfferingComponent } from './templates/professsor-offering/professsor-offering.component';
+import { StudentAllCoursesComponent } from './templates/student-all-courses/student-all-courses.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -16,6 +17,7 @@ const routes: Routes = [
   { path: 'professor/home', component: ProfessorHomeComponent },
   { path: 'professor/offering', component: ProfesssorOfferingComponent },
   { path: 'student/home', component: StudentHomeComponent },
+  { path: 'student/all-courses', component: StudentAllCoursesComponent },
 ];
 
 @NgModule({
