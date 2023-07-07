c57372f53301bbbc684fa00dd6ce3eb1d7fb0f75
RogerSGomes <rogersg.dev@gmail.com>
2023-07-07 11:50:54 -0300

    FEAT: course-detail template ended

--- src/app/shared/components/accordion/accordion.component.ts
+++ src/app/shared/components/accordion/accordion.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ContentChildren, Input, QueryList } from '@angular/core';
 
 @Component({
   selector: 'app-accordion',
   templateUrl: './accordion.component.html',
-  styleUrls: ['./accordion.component.css']
+  styleUrls: ['./accordion.component.css'],
 })
 export class AccordionComponent {
+  isOpened: boolean = false;
 
+  @Input()
+  title: string = '';
+
+  @Input()
+  content: string = '';
 }
