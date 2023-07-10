2d8fca10e0eb72660eea9d1dcfce519b46acceb5
RobertSGomes <robertsg.dev@gmail.com>
2023-07-10 16:37:03 -0300

    FEAT: Forms da Step2 iniciados

---
+++ src/app/templates/professor-new-offering/components/step-two/step-two-form-five/step-two-form-five.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-step-two-form-five',
+  templateUrl: './step-two-form-five.component.html',
+  styleUrls: ['./step-two-form-five.component.css']
+})
+export class StepTwoFormFiveComponent {
+
+}
---
+++ src/app/templates/professor-new-offering/components/step-two/step-two-form-one/step-two-form-one.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-step-two-form-one',
+  templateUrl: './step-two-form-one.component.html',
+  styleUrls: ['./step-two-form-one.component.css']
+})
+export class StepTwoFormOneComponent {
+
+}
