aaec82b4f35de176455eeeaed7ed3ce514615cc0
RobertSGomes <robertsg.dev@gmail.com>
2023-07-10 13:59:03 -0300

    FEAT: Home com curso registrado finalizada

--- src/app/templates/student-home/student-home.component.ts
+++ src/app/templates/student-home/student-home.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+type RegisteredCourse = {
+  sigla: string;
+  oferecimento: string;
+  nome: string;
+  tipo: string;
+  inscricao: string;
+};
+
 @Component({
   selector: 'app-student-home',
   templateUrl: './student-home.component.html',
-  styleUrls: ['./student-home.component.css']
+  styleUrls: ['./student-home.component.css'],
 })
 export class StudentHomeComponent {
-
+  registeredCourses: RegisteredCourse[] = [
+    // {
+    //   sigla: 'BIO-0083',
+    //   oferecimento: '003',
+    //   nome: 'BIOQUÍMICA E FISIOLOGIA APLICADAS AO TREINAMENTO FÍSICO',
+    //   tipo: 'Curso de extensão',
+    //   inscricao: 'INCOMPLETA',
+    // },
+  ];
 }
