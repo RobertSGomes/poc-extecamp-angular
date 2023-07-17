e3dc292bef8a3472bda3dda3a7bacfb6af884bb2
RobertSGomes <robertsg.dev@gmail.com>
2023-07-17 17:00:21 -0300

    FEAT: Validação e inseeção de novos professores da Unicamp

--- src/app/features/professor/templates/professor-new-offering/types/professor.type.ts
+++ src/app/features/professor/templates/professor-new-offering/types/professor.type.ts
@@ -1,11 +1,13 @@
 export type Professor = {
-  matricula: string;
   docente: string;
-  instituicao: string;
-  unidade: string;
-  departamento: string;
-  titulacao: string;
-  carga_horas: string;
-  carga_minutos: string;
+  docente_carga_horaria_horas: string;
+  docente_carga_horaria_minutos: string;
+  docente_departamento: string;
+  docente_id: string;
+  docente_matricula: string;
+  docente_nome: string;
+  docente_situacao: string;
+  docente_titulacao: string;
+  docente_unidade: string;
   vinculo?: string;
 };
