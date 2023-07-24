9539451ae2948bf7accd64fab9d584f93158f2e0
RobertSGomes <robertsg.dev@gmail.com>
2023-07-24 14:01:39 -0300

    FEAT: Environments development

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  baseURLAuth: '',
-  baseURLCourse: '',
-  baseURLProfessor: '',
-  baseURLStudent: '',
+  baseURLAuth: 'http://localhost:3002/auth',
+  baseURLCourse: 'http://localhost:3003/cursos',
+  baseURLProfessor: 'http://localhost:3000/professores',
+  baseURLStudent: 'http://localhost:3001/alunos',
 };
